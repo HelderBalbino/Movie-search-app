4e809aba9e92cd1a4fc3b3bafedca52556c88a31
Helder Balbino <helderbalbino@googlemail.com>
2025-02-14 10:02:57 +0000

    Update API URL in searchMovies function to use HTTPS

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function searchMovies() {
 	resultsDiv.innerHTML = '';
 	loadingText.classList.remove('hidden');
 
-	let apiUrl = `http://www.omdbapi.com/?s=${query}&apikey=cfe0c5c4`;
+	let apiUrl = `https://www.omdbapi.com/?s=${query}&apikey=cfe0c5c4`;
 
 	fetch(apiUrl)
 		.then((response) => response.json())
